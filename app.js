892f488c39db21b9efac647189e0f5f57a74335d
Cody Frank <cody.frank30@gmail.com>
2020-06-06 19:49:05 -0400

    sets up donenv and database connection

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require('express')
 
 const app = express()
 
+const mongoose = require('mongoose')
+
+require('dotenv/config')
+
 // routes -----------
 
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
+
+app.get('/posts', (req, res) => {
+    res.send('Posts')
+})
+
+// connect to db 
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, console.log("connected to db"))
+
+// listen to server
 
 app.listen(3000)
