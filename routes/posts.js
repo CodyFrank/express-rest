a7e2afafcbbbf7f307f8d6c1ba5c3afe3fe3f171
Cody Frank <cody.frank30@gmail.com>
2020-06-07 13:38:33 -0400

    sets up posts post and sends content to database

--- routes/posts.js
+++ routes/posts.js
@@ -9,7 +9,16 @@ router.get("/", (req, res) => {
 })
 
 router.post("/", (req, res) => {
-    console.log(req.body)
+    const post = new Post(
+        {
+            title: req.body.title,
+            content: req.body.content
+        }
+    )
+
+    post.save()
+    .then(data => res.json(data))
+    .catch(err => res.json(err))
 })
 
 
