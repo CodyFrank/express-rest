62dcfec91c04eed5105c1615f23b11960477ba0f
Cody Frank <cody.frank30@gmail.com>
2020-06-06 20:14:33 -0400

    completes posts route transfer to own file

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const mongoose = require('mongoose')
 
 require('dotenv/config')
 
+// import routes
+const postsRoute = require('./routes/posts')
+
+// middleware
+app.use('/posts', postsRoute)
+
+// home route
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
 // connect to db 
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, console.log("connected to db"))
--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,9 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send("Hello World")
+router.get("/", (req, res) => {
+    res.send("posts")
 })
 
-router.get('/posts', (req, res) => {
-    res.send("Posts")
-})
+
+module.exports = router
